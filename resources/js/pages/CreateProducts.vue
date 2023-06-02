a1e802209efa47bcafee3c3a68e5fb09dad26fa0
Patrick Marcon Concepcion <pmconcepcion1201@gmail.com>
2023-06-02 19:07:47 +0800

    setup for first step

--- resources/js/pages/CreateProducts.vue
+++ resources/js/pages/CreateProducts.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        create
+    <div class="container">
+        <div>
+            <div class="card">
+                
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    name: 'Create Products',
+
 }
 </script>
 <style scoped>
